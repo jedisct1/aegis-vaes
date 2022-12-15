7433d59496f44810affb8101ca6abd84d83499fa
Frank Denis <github@pureftpd.org>
2022-12-15 18:49:31 +0100

    Reorder

--- aead_aegis128l_vaes.c
+++ aead_aegis128l_vaes.c
@@ -33,14 +33,14 @@
 static inline void
 aegis128l_update(__m256i *const state, const __m256i d)
 {
-    const __m256i t51 = _mm256_permute2x128_si256(state[1], state[1], 0x03);
     const __m256i t26 = _mm256_permute2x128_si256(state[0], state[0], 0x03);
+    const __m256i t51 = _mm256_permute2x128_si256(state[1], state[1], 0x03);
     const __m256i t73 = _mm256_permute2x128_si256(state[2], state[2], 0x03);
     const __m256i t04 = state[3];
 
     state[1] = _mm256_aesenc_epi128(t04, state[1]);
-    state[0] = _mm256_aesenc_epi128(t51, state[0]);
     state[2] = _mm256_aesenc_epi128(t26, state[2]);
+    state[0] = _mm256_aesenc_epi128(t51, state[0]);
     state[3] = _mm256_aesenc_epi128(t73, state[3]);
 
     state[3] = _mm256_xor_si256(state[3], d);
@@ -125,8 +125,8 @@ aegis128l_enc(unsigned char *const dst, const unsigned char *const src, __m256i 
     AES_BLOCK_STORE2(dst, t);
 
     state[1] = _mm256_aesenc_epi128(state[3], state[1]);
-    state[0] = _mm256_aesenc_epi128(t51, state[0]);
     state[2] = _mm256_aesenc_epi128(t26, state[2]);
+    state[0] = _mm256_aesenc_epi128(t51, state[0]);
     state[3] = _mm256_aesenc_epi128(t73, state[3]);
 
     state[3] = _mm256_xor_si256(state[3], msg);
@@ -152,8 +152,8 @@ aegis128l_dec(unsigned char *const dst, const unsigned char *const src, __m256i 
     AES_BLOCK_STORE2(dst, t);
 
     state[1] = _mm256_aesenc_epi128(state[3], state[1]);
-    state[0] = _mm256_aesenc_epi128(t51, state[0]);
     state[2] = _mm256_aesenc_epi128(t26, state[2]);
+    state[0] = _mm256_aesenc_epi128(t51, state[0]);
     state[3] = _mm256_aesenc_epi128(t73, state[3]);
 
     state[3] = _mm256_xor_si256(state[3], t);
